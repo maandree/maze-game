7254f4c1133675e6755bd2e2933e1215b0074f44
Mattias Andrée <maandree@operamail.com>
2012-10-22 16:41:04 +0200

    The main method skeleton

---
+++ src/se/kth/maandree/mazegame/Program.java
@@ -0,0 +1,31 @@
+package se.kth.maandree.mazegame;
+
+
+/**
+ * This is the main class of the program
+ * 
+ * @author  Mattias Andrée, <a href="mailto:maandree@kth.se">maandree@kth.se</a>
+ */
+public class Program
+{
+    /**
+     * Non-constructor
+     */
+    private Program()
+    {
+	assert false : "You are not meant to initialise this class [Program],";
+    }
+    
+    
+    
+    /**
+     * This is the main entry point of the porgram
+     * 
+     * @param  args  Command line arguments
+     */
+    public static void main(final String... args)
+    {
+    }
+
+}
+
